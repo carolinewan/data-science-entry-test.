76a34a62b9a5cdf1b37309cd85336d78668e2498
carolinewan <djcarrie2006@gmail.com>
2025-06-12 12:38:04 +0800

    Update Q2.py

--- Q2.py
+++ Q2.py
@@ -1,11 +1,11 @@
 #def find_and_replace(lst, find_val, replace_val):
-    """
-    Task 1
-    - Create a function that searches for all occurrences of a value (find_val) in a given list (lst) and replaces them with another value (replace_val).
-    - lst must be a list.
-    - Return the modified list.
-    """
-    return
+#    """
+#    Task 1
+#    - Create a function that searches for all occurrences of a value (find_val) in a given list (lst) and replaces them with another value (replace_val).
+#   - lst must be a list.
+#    - Return the modified list.
+#    """
+#    return
 
 
 # Task 2
